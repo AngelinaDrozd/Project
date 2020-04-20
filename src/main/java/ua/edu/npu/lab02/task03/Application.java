ac60c0e792ed5453753dc9dab1c67d7b59d640de
USER <19fi.a.drozd@std.npu.edu.ua>
2020-04-20 20:57:51 +0300

    Third task lab02

---
+++ src/main/java/ua/edu/npu/lab02/task03/Application.java
@@ -0,0 +1,8 @@
+package ua.edu.npu.lab02.task03;
+
+public class Application {
+    public static void main(String[] args) {
+        Dog dog = new Dog("Rex", 4);
+        Dog dog1 = new Dog();
+    }
+}
