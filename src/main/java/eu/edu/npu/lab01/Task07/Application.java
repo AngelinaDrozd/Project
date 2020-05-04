2477167fc969d5649ac842945191207eae9dedbd
USER <19fi.a.drozd@std.npu.edu.ua>
2020-05-04 12:29:56 +0300

    changes

---
+++ src/main/java/eu/edu/npu/lab01/Task07/Application.java
@@ -0,0 +1,10 @@
+package eu.edu.npu.lab01.Task07;
+
+public class Application {
+    public static void main(String[] args) {
+        Redaction Strings = new Redaction();
+        Strings.StringInput();
+        Strings.Redact();
+        Strings.out();
+    }
+}
