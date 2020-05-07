73921b36fe20ec187f122d130cf515f57acc88aa
USER <19fi.a.drozd@std.npu.edu.ua>
2020-05-07 20:26:47 +0300

    Some changes

--- src/main/java/eu/edu/npu/lab04/Task02/Stack.java
+++
@@ -1,9 +0,0 @@
-package eu.edu.npu.lab04.Task02;
-
-public interface Stack {
-    public void push (int data);
-    public int pop ();
-    public int size ();
-    public boolean isEmpty();
-    public void clear ();
-}
