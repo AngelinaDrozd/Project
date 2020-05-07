2df4c343d127ed9d130c23be4711babf98164faf
USER <19fi.a.drozd@std.npu.edu.ua>
2020-05-07 19:55:01 +0300

    Third task lab03

---
+++ src/main/java/eu/edu/npu/lab03/Task03/Dog.java
@@ -0,0 +1,34 @@
+package eu.edu.npu.lab03.Task03;
+
+public class Dog extends Pet {
+    private int age ;
+    private String name;
+
+    public Dog(String name, int age){
+        this.name = name;
+        this.age = age;
+        System.out.println("Name is "+ name + " and age are " + age + " years!");
+    }
+    public Dog( ){
+        System.out.println("Name is unknown and its age is 0 !");
+    }
+    public void setName (String name){
+        this.name = name;
+    }
+    public String getName(){
+        return name;
+    }
+    public void setAge (int age){
+        this.age = age;
+    }
+    public int getAge(){
+        return age;
+    }
+
+
+    public void said(){
+        for ( int i=0; i<age; i++){
+            System.out.print("gav ");
+        }
+    }
+}
