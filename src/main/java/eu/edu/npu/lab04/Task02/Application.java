ed2cb4c236d4f984fd819ffbe8aa1d40c04a3ad2
USER <19fi.a.drozd@std.npu.edu.ua>
2020-05-07 20:29:46 +0300

    Second task lab04

---
+++ src/main/java/eu/edu/npu/lab04/Task02/Application.java
@@ -0,0 +1,21 @@
+package eu.edu.npu.lab04.Task02;
+
+public class Application {
+    public static void main(String[] args) {
+        ArrayStack stack = new ArrayStack();
+        // Добавление элементов
+        System.out.println("Сформирован стек st1: ");
+        stack.push(9);
+        stack.push(8);
+        stack.push(7);
+        stack.push(6);
+        stack.push(5);
+        stack.push(4);
+        stack.push(3);
+        stack.push(2);
+        stack.push(1);
+        stack.push(0);
+        System.out.println();
+
+    }
+}
