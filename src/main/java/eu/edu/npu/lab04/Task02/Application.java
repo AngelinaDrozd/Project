f37d454d0c7cec6aaaa4d88760f8543bdd040b4a
USER <19fi.a.drozd@std.npu.edu.ua>
2020-05-07 20:20:30 +0300

    Second task lab04

---
+++ src/main/java/eu/edu/npu/lab04/Task02/Application.java
@@ -0,0 +1,20 @@
+package eu.edu.npu.lab04.Task02;
+
+public class Application {
+    public static void main(String[] args) {
+        ArrayStack stack = new ArrayStack();
+        System.out.println("Cтек st1: ");
+        stack.push(9);
+        stack.push(8);
+        stack.push(7);
+        stack.push(6);
+        stack.push(5);
+        stack.push(4);
+        stack.push(3);
+        stack.push(2);
+        stack.push(1);
+        stack.push(0);
+        System.out.println();
+
+    }
+}
---
+++ src/main/java/eu/edu/npu/lab04/Task02/Stack.java
@@ -0,0 +1,9 @@
+package eu.edu.npu.lab04.Task02;
+
+public interface Stack {
+    public void push (int data);
+    public int pop ();
+    public int size ();
+    public boolean isEmpty();
+    public void clear ();
+}
